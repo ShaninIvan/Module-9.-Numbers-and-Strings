4101224e68d4d232d55ee351e5e2bd4a75b96f98
ShaninIvan <shaninivan1993@gmail.com>
2020-08-31 16:09:40 +0300

    update

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ let log = null;
 const inputWindow = document.getElementById('inputWindow');
 
 //Приведение к вещественному типу
-function format(){
+function format() {
     inputWindow.value = parseFloat(inputWindow.value);
 }
 
 //Отображение текущих операнда и операции
-function action(lastOperand, operation){
+function action(lastOperand, operation) {
     document.querySelector('.action').innerText = lastOperand + operation;
+}
+
+//Обновление журнала
+function updateJournal(operandB, result){
+    document.querySelector(".journal ul").innerHTML += "<li>" + lastOperand + operation + operandB + "=" + result +"</li>" +"<hr>";
 }
 
 //Очистка
@@ -19,11 +24,12 @@ document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '0';
-    action("","");
+    action("", "");
+    document.querySelector(".journal ul").innerHTML = "";
 })
 
 //BACKSPACE
-document.querySelector("#btn_backspace").addEventListener('click', function(){
+document.querySelector("#btn_backspace").addEventListener('click', function () {
     inputWindow.value = inputWindow.value.slice(0, -1);
     if (inputWindow.value.length == 0) {
         inputWindow.value = "0";
@@ -31,40 +37,42 @@ document.querySelector("#btn_backspace").addEventListener('click', function(){
 })
 
 //Ввод чисел
-document.querySelector('.card-body').addEventListener('click', function(event) {
+document.querySelector('.card-body').addEventListener('click', function (event) {
     let targ = event.target;
-    if (targ.classList.contains('number')){
+    if (targ.classList.contains('number')) {
         inputWindow.value += targ.innerText;
         format();
     }
 })
 
 //Точка
-document.querySelector("#btn_point").addEventListener('click', function(){
-        if (inputWindow.value.includes('.') == false) {
-            inputWindow.value += ".";
+document.querySelector("#btn_point").addEventListener('click', function () {
+    if (inputWindow.value.includes('.') == false) {
+        inputWindow.value += ".";
     }
 })
 
 //Унарный минус
-document.querySelector("#btn_negative").addEventListener('click', function(){
-        inputWindow.value = inputWindow.value * (-1);
+document.querySelector("#btn_negative").addEventListener('click', function () {
+    inputWindow.value = inputWindow.value * (-1);
 })
 
 //Квадратный корень
-document.querySelector("#btn_sqrt").addEventListener('click', function(){
-    if (inputWindow.value<0){
+document.querySelector("#btn_sqrt").addEventListener('click', function () {
+    var val = inputWindow.value;
+    if (val < 0) {
         alert('Нельзя извлечь квадратный корень из отрицательного числа');
     }
-    else{
-        inputWindow.value = Math.sqrt(inputWindow.value);
+    else {
+        inputWindow.value = Math.sqrt(val);
+        document.querySelector(".journal ul").innerHTML += "<li>" + "sqrt(" + val + ")=" + inputWindow.value +"</li>"+"<hr>";
     }
-    
+
 })
 
 //Сложение
-document.querySelector("#btn_plus").addEventListener('click', function(){
-    if (operation === null){
+document.querySelector("#btn_plus").addEventListener('click', function () {
+    if (operation === null) {
         lastOperand = parseFloat(inputWindow.value);
         operation = '+';
         inputWindow.value = 0;
@@ -73,8 +81,8 @@ document.querySelector("#btn_plus").addEventListener('click', function(){
 })
 
 //Сложение
-document.querySelector("#btn_minus").addEventListener('click', function(){
-    if (operation === null){
+document.querySelector("#btn_minus").addEventListener('click', function () {
+    if (operation === null) {
         lastOperand = parseFloat(inputWindow.value);
         operation = '-';
         inputWindow.value = 0;
@@ -83,8 +91,8 @@ document.querySelector("#btn_minus").addEventListener('click', function(){
 })
 
 //Умножение
-document.querySelector("#btn_multiply").addEventListener('click', function(){
-    if (operation === null){
+document.querySelector("#btn_multiply").addEventListener('click', function () {
+    if (operation === null) {
         lastOperand = parseFloat(inputWindow.value);
         operation = '*';
         inputWindow.value = 0;
@@ -93,8 +101,8 @@ document.querySelector("#btn_multiply").addEventListener('click', function(){
 })
 
 //Деление
-document.querySelector("#btn_divide").addEventListener('click', function(){
-    if (operation === null){
+document.querySelector("#btn_divide").addEventListener('click', function () {
+    if (operation === null) {
         lastOperand = parseFloat(inputWindow.value);
         operation = '/';
         inputWindow.value = 0;
@@ -104,33 +112,39 @@ document.querySelector("#btn_divide").addEventListener('click', function(){
 
 
 //Вычисление
-document.querySelector("#btn_calc").addEventListener('click', function(){
-    if (operation != null){
+document.querySelector("#btn_calc").addEventListener('click', function () {
+    if (operation != null) {
         let val = parseFloat(inputWindow.value);
 
-        if (val == 0 && operation == "/"){
+        if (val == 0 && operation == "/") {
             alert('Деление на 0 невозможно');
             return false;
         }
 
-        switch(operation){
+        switch (operation) {
             case '+':
-                val = lastOperand + val;
+                result = lastOperand + val;
                 break;
             case '-':
-                val = lastOperand - val;
+                result = lastOperand - val;
                 break;
             case '*':
-                val = lastOperand * val;
+                result = lastOperand * val;
                 break;
             case '/':
-                val = lastOperand / val;
+                result = lastOperand / val;
                 break;
         }
 
-        inputWindow.value = val;
+        inputWindow.value = result;
+        updateJournal(val, result)
         lastOperand = 0;
         operation = null;
-        action("","");
+        action("", "");
     }
+})
+
+//Журнал
+document.querySelector("#btn_journal").addEventListener('click', function () {
+    document.querySelector(".journal").classList.toggle('journal-show');
 })
